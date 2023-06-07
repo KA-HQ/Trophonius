25e647ea5465035c23f95b35a0f20e0d6948970c
ArthurWeiden <6594565+ArthurWeiden@users.noreply.github.com>
2023-06-07 14:42:36 +0200

    rm time test active_support

--- lib/time.rb
+++ lib/time.rb
@@ -1,7 +1,7 @@
-require 'time'
-class Time
-  def to_fm
-    strftime('%m-%d-%Y %H:%M:%S')
-  end
-  alias convert_to_fm to_fm
-end
+# require 'time'
+# class Time
+#   def to_fm
+#     strftime('%m-%d-%Y %H:%M:%S')
+#   end
+#   alias convert_to_fm to_fm
+# end
