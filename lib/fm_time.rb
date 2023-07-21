04b6a4c5b43b0812ddfca14ffa333ac2a81eacb7
ArthurWeiden <6594565+ArthurWeiden@users.noreply.github.com>
2023-07-21 10:46:03 +0200

    added time.fromfm

--- lib/fm_time.rb
+++ lib/fm_time.rb
@@ -2,5 +2,10 @@ class Time
   def to_fm
     strftime('%m-%d-%Y %H:%M:%S')
   end
+
+  def self.from_fm(time)
+    Time.strptime(time, '%m/%d/%Y %H:%M:%S')
+  end
+
   alias convert_to_fm to_fm
 end
