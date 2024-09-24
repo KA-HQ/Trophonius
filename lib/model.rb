9fd3659e05d24d8e17649793e9d6d67e13fb8bc2
ArthurWeiden <6594565+ArthurWeiden@users.noreply.github.com>
2024-09-24 13:41:45 +0200

    puts args

--- lib/model.rb
+++ lib/model.rb
@@ -45,7 +45,8 @@ module Trophonius
     def self.scope(name, procedure, *_args)
       define_singleton_method(name) do |*args|
         puts procedure.arity
-        puts procedure.call(args)
+        p procedure.call(args)
+        p args
         procedure.arity.zero? ? procedure.call : procedure.call(args)
       end
     end
