2ab48ef4208e6707f4eef6e8977b40865aad50b3
ArthurWeiden <6594565+ArthurWeiden@users.noreply.github.com>
2026-03-30 10:50:02 +0200

    translator method added

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,5 +11,11 @@ module Trophonius
     def methodize_portal_field(field_name)
       ActiveSupport::Inflector.parameterize(ActiveSupport::Inflector.underscore(field_name.gsub(/\w+::/, '').to_s), separator: '_')
     end
+
+    def portal_relation_name(first_related_record)
+      return '' if first_related_record.nil?
+
+      first_related_record.keys.map{ |f| f[/.*(?=::)/] }.tally.max_by(&:last).first
+    end
   end
 end
